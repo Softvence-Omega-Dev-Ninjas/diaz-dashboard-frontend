97e4520f2170706ca74681ca8dc52f322441ae6c
Md Habib Ullah <habibhk127@gmail.com>
2025-12-10 03:07:31 +0600

    Completed Overview Page Integration

--- src/components/Overview/OverviewCards.tsx
+++ src/components/Overview/OverviewCards.tsx
@@ -1,20 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { LuCircleCheckBig, LuShip, LuStar, LuUserCheck } from 'react-icons/lu';
 
-const OverviewCards = () => {
+const OverviewCards = ({ cardsData }: any) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5 py-6">
       <div className="flex items-center gap-3 justify-between bg-[#003235] text-white px-5 xl:px-3 2xl:px-6 py-4 rounded-lg">
         <div className="space-y-1">
           <p className="text-xs">Total Yachts Listed</p>
-          <h1 className="text-xl">1.247</h1>
-          <p className="text-xs">+12% from last month</p>
+          <h1 className="text-xl">{cardsData.totalYachts}</h1>
+          <p className="text-xs">+{cardsData.totalYatchPercentageChange}% from last month</p>
         </div>
         <LuShip className="h-8 w-8 text-white" />
       </div>
       <div className="flex items-center gap-3 justify-between bg-[#00555A] text-white px-5 xl:px-3 2xl:px-6 py-4 rounded-lg">
         <div className="space-y-1">
           <p className="text-xs">Pending Approvals</p>
-          <h1 className="text-xl">23</h1>
+          <h1 className="text-xl">{cardsData.pendingApprovals}</h1>
           <p className="text-xs">Needs attention</p>
         </div>
         <LuCircleCheckBig className="h-8 w-8 text-white" />
@@ -22,7 +23,7 @@ const OverviewCards = () => {
       <div className="flex items-center gap-3 justify-between bg-[#007B82] text-white px-5 xl:px-3 2xl:px-6 py-4 rounded-lg">
         <div className="space-y-1">
           <p className="text-xs">Verified Sellers</p>
-          <h1 className="text-xl">342</h1>
+          <h1 className="text-xl">{cardsData.verifiedSellers}</h1>
           <p className="text-xs">+8 this week</p>
         </div>
         <LuUserCheck className="h-8 w-8 text-white" />
@@ -30,7 +31,7 @@ const OverviewCards = () => {
       <div className="flex items-center gap-3 justify-between bg-[#00A3AC] text-white px-5 xl:px-3 2xl:px-6 py-4 rounded-lg">
         <div className="space-y-1">
           <p className="text-xs">Featured Yachts</p>
-          <h1 className="text-xl">48</h1>
+          <h1 className="text-xl">{cardsData.featuredYachts}</h1>
           <p className="text-xs">Active listings</p>
         </div>
         <LuStar className="h-8 w-8 text-white" />
---
+++ src/redux/features/auth/authApi.ts
@@ -0,0 +1,80 @@
+import { baseApi } from "@/redux/api/baseApi";
+
+
+const authApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    emailLogin: build.mutation({
+      query: (credentials) => ({
+        url: "/auth/login/email",
+        method: "POST",
+        body: credentials,
+      }),
+    }),
+    varifyemailLogin: build.mutation({
+      query: (credentials) => ({
+        url: "/auth/verify/email",
+        method: "POST",
+        body: credentials,
+      }),
+    }),
+    phoneLogin: build.mutation({
+      query: (credentials) => ({
+        url: "/auth/login/phone",
+        method: "POST",
+        body: credentials,
+      }),
+    }),
+    varifyPhoneLogin: build.mutation({
+      query: (credentials) => ({
+        url: "/auth/verify/phone",
+        method: "POST",
+        body: credentials,
+      }),
+    }),
+    superAdminLogin: build.mutation({
+      query: (credentials) => ({
+        url: "/auth/login/super-admin",
+        method: "POST",
+        body: credentials,
+      }),
+    }),
+    updateProfile: build.mutation({
+      query: (credentials) => ({
+        url: `/employee/user/profile`,
+        method: "PATCH",
+        body: credentials?.formData,
+      }),
+    }),
+    updateRole: build.mutation({
+      query: (credentials) => ({
+        url: `/admin/user/${credentials?.userId}/role`,
+        method: "PATCH",
+        body: credentials?.data,
+      }),
+      invalidatesTags: ["User"],
+    }),
+    getProfile: build.query({
+      query: (credentials) => ({
+        url: `/admin/user/id/${credentials}`,
+      }),
+    }),
+    getUserProfile: build.query({
+      query: () => ({
+        url: `/admin/user/me/profile`,
+        method: "GET",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useEmailLoginMutation,
+  useSuperAdminLoginMutation,
+  useVarifyemailLoginMutation,
+  usePhoneLoginMutation,
+  useVarifyPhoneLoginMutation,
+  useUpdateProfileMutation,
+  useGetProfileQuery,
+  useUpdateRoleMutation,
+  useGetUserProfileQuery
+} = authApi;
